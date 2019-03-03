50368a65ca0092294ad5de18730a53eb5c5c0d93
wyangy <wyangynz@gmail.com>
2019-03-03 23:19:21 +1300

    add "add user" button and alert

--- dude/ChatsViewController.swift
+++ dude/ChatsViewController.swift
@@ -13,6 +13,55 @@ import FirebaseAuth
 class ChatsViewController: UIViewController {
 
     @IBOutlet weak var usersTableView: UITableView!
+    
+    @IBAction func addUser(_ sender: Any) {
+        var userEmailTextField: UITextField?
+        
+        let alertController = UIAlertController(
+            title: "Add user",
+            message: "Please enter the email address of the user you want to add to your chats",
+            preferredStyle: .alert)
+        
+        alertController.addTextField {
+            (textUserEmail) -> Void in
+            userEmailTextField = textUserEmail
+            userEmailTextField!.placeholder = "User's email here"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let addUserAction = UIAlertAction(
+        title: "Add", style: .default) {
+            (action) -> Void in
+            
+//            if let username = userEmailTextField?.text {
+//                print("added user: \(username)")
+//            } else {
+//                print("No Username entered")
+//            }
+            
+            
+            if (userEmailTextField?.text?.isEmpty)! {
+                let alertController = UIAlertController(title: "Error", message: "User's email address cannot be empty", preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil) // problem here is that after pressing cancel, it did not go back to text field alert controller
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                print("user added: \(userEmailTextField!.text!)")
+            }
+            
+            
+            
+        }
+        
+        
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(addUserAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func logOut(_ sender: Any) {
         do {
             try Auth.auth().signOut()
